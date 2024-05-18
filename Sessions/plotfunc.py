6aa71bf6b806f423d12251188a2881b24a1cb8a7
JoseFerreira99 <150545522+JoseFerreira99@users.noreply.github.com>
2024-05-18 11:31:13 +0100

    Test
    EVerything working, missing exception for when map ends

--- Sessions/plotfunc.py
+++ Sessions/plotfunc.py
@@ -11,52 +11,54 @@ lon_point = []
 lat_point = []
 risks = []
 
-
 lons = []
 lats = []
 # Read the csv file and extract data
+with open(filepath_1, 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)  # Skip the header
+    for row in csv_reader:
+        lon = float(row[2])
+        lat = float(row[3])
+        lons.append(lon)
+        lats.append(lat)
+
+
 with open(filepath, 'r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader)  # Skip the header
     next(csv_reader)  # Skip the second line
     for row in csv_reader:
-        center_lon = float(row[1].replace('(', ''))
-        center_lat = float(row[2].replace(')', ''))
-        risk = float(row[3].replace(')', ''))
-        point_lon = float(row[4])
-        point_lat = float(row[5])
+    
+        center_lon = float(row[1].replace('(', '').replace("'", '').strip())
+        center_lat = float(row[2].replace(')', '').replace("'", '').strip())
+        risk = float(row[3].replace(')', '').replace("'",'').strip())
+        point_lon = float(row[4].strip())
+        point_lat = float(row[5].strip())
+        
         lon_center.append(center_lon)
         lat_center.append(center_lat)
         lon_point.append(point_lon)
         lat_point.append(point_lat)
         risks.append(risk)
-"""
-with open(filepath_1, 'r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        center_lon = float(row[2])
-        center_lat = float(row[3])
 
-        lons.append(center_lon)
-        lats.append(center_lat)
-        lon_point.append(point_lon)
-        lat_point.append(point_lat)
-"""
 
 # Plotting with different colors based on risk
 plt.figure(figsize=(10, 6))
 for i, risk in enumerate(risks):
     if risk == 1.0:
-        plt.plot(lon_center[i], lat_center[i], 'gx', color='darkgreen')
-        plt.plot(lon_point[i], lat_point[i], 'go',  color='lightgreen')
+        plt.scatter(lon_center[i], lat_center[i], color='darkgreen', marker='x')
+        plt.scatter(lon_point[i], lat_point[i], color='lightgreen', marker='o')
     elif risk == 2.0:
-        plt.plot(lon_center[i], lat_center[i], 'yx', color='goldenrod')
-        plt.plot(lon_point[i], lat_point[i], 'yo',  color='yellow')
+        plt.scatter(lon_center[i], lat_center[i], color='goldenrod', marker='x')
+        plt.scatter(lon_point[i], lat_point[i], color='yellow', marker='o')
     elif risk == 3.0:
-        plt.plot(lon_center[i], lat_center[i], 'rx', color='red')
-        plt.plot(lon_point[i], lat_point[i], 'ro',  color='red')
+        plt.scatter(lon_center[i], lat_center[i], color='red', marker='x')
+        plt.scatter(lon_point[i], lat_point[i], color='red', marker='o')
 
-#plt.plot(lons,lats, color = 'lightgrey')
+
+
+plt.plot(lons,lats, color = 'lightgrey')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 plt.title('Cyclist Trajectory and Respective Centers')
